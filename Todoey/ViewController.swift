f40677288727bab2862a642996d0ce75ce1179c3
TommyRchmt <tom.rchmt02@gmail.com>
2019-11-17 11:15:24 +0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Tommy Rachmat on 17/11/19.
+//  Copyright © 2019 Tommy Rachmat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
